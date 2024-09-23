21717e5f4fa048f689bba6cd9918d309d001a76f
Ben Do <bdo.appworkshop@gmail.com>
2024-09-23 14:37:47 -0500

    Implemented ResultsPage.

---
+++ pages/results/index.tsx
@@ -0,0 +1,11 @@
+import Results from "../../components/results/results";
+const ResultsPage = () => {
+
+    return (
+        <>
+        <Results />
+        </>
+    );
+};
+
+export default ResultsPage;
