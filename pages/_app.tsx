09b07c980fe1853ad8264690474ed76c31d748d2
Ben Do <bdo.appworkshop@gmail.com>
2024-09-23 14:36:41 -0500

    Inserted NavigationBar above child components.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import "bootstrap/dist/css/bootstrap.min.css"; // Import Bootstrap CSS
+import NavigationBar from "../components/NavigationBar/navigation-bar";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <NavigationBar />
+      <Component {...pageProps} />
+    </>
+  );
 }
