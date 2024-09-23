5008bc794b4b73d5fbe16f6ade79e1ec3bc03274
Ben Do <bdo.appworkshop@gmail.com>
2024-09-23 14:37:19 -0500

    Implemented CalcChoicePage.

---
+++ pages/calc-choice/index.tsx
@@ -0,0 +1,12 @@
+import ChoiceCalc from "../../components/ChoiceCalc/choice-calc";
+
+const CalcChoicePage = () => {
+
+    return (
+        <>
+        <ChoiceCalc />
+        </>
+    );
+};
+
+export default CalcChoicePage;
